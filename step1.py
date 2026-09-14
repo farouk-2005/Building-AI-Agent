52484ef7f98e4f22707e905c0c1d659ee270ac9d
touka <toukatounakti@gmail.com>
2026-09-14 21:59:54 +0100

    Second commit(final version of step1)

--- step1.py
+++ step1.py
@@ -1,10 +1,32 @@
 from dotenv import load_dotenv
 import os
 from google import genai
-import os
-from dotenv import find_dotenv, load_dotenv
 
-dotenv_path = find_dotenv() #find .env file
-load_dotenv(dotenv_path) #return True if there is envir vars
+load_dotenv()
+MODEL = os.getenv("MODEL_NAME")
 
-API_KEY = os.getenv("API_KEY")
+client = genai.Client(api_key=os.getenv("API_KEY"))
+
+response = client.models.generate_content(
+    model=MODEL,
+    contents="Hi my name is Farouk."
+)
+print(response.text)
+
+response = client.models.generate_content(
+    model=MODEL,
+    contents="Hi whats my name."
+)
+print(response.text)
+
+print(" ===== Multi-turn chat ===== ")
+
+#The chats module keeps conversation history for you automatically:
+chat = client.chats.create(model=MODEL)
+
+r1 = chat.send_message("Hi my name is Farouk.")
+print(r1.text)
+
+r2 = chat.send_message("Say my name")
+print(r2.text)
+
